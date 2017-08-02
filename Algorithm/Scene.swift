20179f8f4f26e3a257667ea5d7dca9e586d3ee97
ObaTakeshi <s153025@matsu.shimane-u.ac.jp>
2017-08-02 18:42:34 +0900

    バブルソート視覚化/微調整

--- Algorithm/Scene.swift
+++ Algorithm/Scene.swift
@@ -15,13 +15,13 @@ class TestScene:SKScene {
     var linearShapeNode: SKShapeNode!
     var i: Int = 0
     var j: Int = 0
-    var start:Bool = false
+    var start: Bool = false
     
     //現在シーン設定時の呼び出しメソッド
     override func didMove(to view: SKView) {
         shokika()
         shuffle()
-        lineDraw()
+        lineDraw(comp1: -1, comp2: -1)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -32,23 +32,26 @@ class TestScene:SKScene {
         if start == true {
             self.removeAllChildren()
             bubblesort()
-            lineDraw()
+            lineDraw(comp1: j, comp2: j-1)
         }
     }
     
     func shokika() {
-        for i in 0...Int(myBoundSize.width / 5.0) {
+        for i in 0...Int(myBoundSize.width / 5.0 - 1) {
             numeric.append(i+1)
         }
         j = numeric.count - 1
     }
     
-    func lineDraw() {
-        for i in 0...Int(myBoundSize.width / 5.0) {
-            var points = [CGPoint(x: i*5, y: 0),
-                          CGPoint(x: i*5, y: numeric[i]*7)]
+    func lineDraw(comp1: Int, comp2: Int) {
+        for m in 0...numeric.count - 1 {
+            var points = [CGPoint(x: Double(m)*5.0+2.5, y: 0),
+                          CGPoint(x: Double(m)*5.0+2.5, y: Double(numeric[m]*7))]
             linearShapeNode = SKShapeNode(points: &points,count: points.count)
             linearShapeNode.lineWidth = 5.0
+            if (m == j || m == i) && start == true {
+                linearShapeNode.strokeColor = UIColor.red
+            }
             self.addChild(linearShapeNode)
         }
     }
@@ -79,12 +82,6 @@ class TestScene:SKScene {
                 }
                 j -= 1
             }
-        }
-    }
-    
-    func arrayPrint() {
-        for i in 0...Int(myBoundSize.width / 5.0) {
-            print(numeric[i])
         }
     }
 }
