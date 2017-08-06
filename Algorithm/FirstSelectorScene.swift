07cf09059ef96bb9c8d2688e8c5aad415407eda0
ObaTakeshi <s153025@matsu.shimane-u.ac.jp>
2017-08-06 20:41:16 +0900

    ソートを選択できるように変更

--- Algorithm/FirstSelectorScene.swift
+++ Algorithm/FirstSelectorScene.swift
@@ -15,13 +15,32 @@ class FirstSelectorScene: SKScene {
         label.text = "Visualized Algorithm"
         label.position = CGPoint(x: (self.view?.frame.midX)!, y: (self.view?.frame.midY)!)
         self.addChild(label)
+        
+        let bubbleLabel = SKLabelNode()
+        bubbleLabel.text = "BubbleSort"
+        bubbleLabel.position = CGPoint(x: (self.view?.frame.maxX)! / 3 * 2, y: (self.view?.frame.maxY)! / 3 * 2)
+        bubbleLabel.name = "BubbleSort"
+        self.addChild(bubbleLabel)
+        
+        let heapLabel = SKLabelNode()
+        heapLabel.text = "HeapSort"
+        heapLabel.position = CGPoint(x: (self.view?.frame.maxX)! / 3, y: (self.view?.frame.maxY)! / 3)
+        heapLabel.name = "HeapSort"
+        self.addChild(heapLabel)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let _ = touches.first as UITouch? {
-            let scene = BubbleSortScene(size:self.frame.size)
-//            let scene = HeapSortScene(size:self.frame.size)
-            self.view?.presentScene(scene)
+        for touch: AnyObject in touches {
+            let location = touch.location(in: self)
+            let touchedNode = self.atPoint(location)
+            
+            if touchedNode.name == "BubbleSort" {
+                let newScene = BubbleSortScene(size: self.frame.size)
+                self.view?.presentScene(newScene)
+            } else if touchedNode.name == "HeapSort" {
+                let newScene = HeapSortScene(size: self.frame.size)
+                self.view?.presentScene(newScene)
+            }
         }
     }
 }
